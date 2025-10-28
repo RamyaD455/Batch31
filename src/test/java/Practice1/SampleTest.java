cdf89282c4c19956b0fc27a5eb4a1760e6f497a6
ramya <ramya@RamyaCatherine>
2025-10-28 10:55:34 +0530

    Committed

---
+++ src/test/java/Practice1/SampleTest.java
@@ -0,0 +1,12 @@
+package Practice1;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {  //class name should end with test bcz we are using @Test
+	@Test(groups="smoke")
+	public void appleTest()
+	{
+		System.out.println("apple tested");
+	}
+
+}
